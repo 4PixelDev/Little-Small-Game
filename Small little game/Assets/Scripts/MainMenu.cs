c50d5fb496dab9cf360c132fa60f8f09618d8665
PixelNass <nasserdevart@gmail.com>
2023-06-21 18:19:55 +0400

    Add main menu
    just making the main menu script, trying to make the whole game loop ,
    its still not finished 100%

---
+++ Small little game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Start Menu");
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting !!!");
+    }
+}
